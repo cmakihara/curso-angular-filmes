bafd43375de61b470b1e7a06f22ef1e329067202
cmakihara <clmakihara@gmail.com>
2021-08-23 10:56:03 -0300

    Interfaces.

---
+++ src/app/shared/models/config-params.ts
@@ -0,0 +1,11 @@
+import { CampoGenerico } from "./campo-generico";
+
+export interface ConfigParams {
+
+    pagina?: number;
+    limite?: number;
+    pesquisa?: string;
+    campo?: CampoGenerico;
+
+
+}
